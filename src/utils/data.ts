eaabcbdc0a553945ae19fce6a1d7e64befa05142
Aswin P C <aswinpc9@gmail.com>
2024-08-06 19:32:53 +0530

    Quiz app started

---
+++ src/utils/data.ts
@@ -0,0 +1,7 @@
+export const QUESTIONS = [
+    { question: 'What is the capital of France?', options: ['Berlin', 'Madrid', 'Paris', 'Rome'], answer: 'Paris' },
+    { question: 'Which planet is known as the Red Planet?', options: ['Earth', 'Mars', 'Jupiter', 'Venus'], answer: 'Mars' },
+    { question: 'Who wrote "Romeo and Juliet"?', options: ['Mark Twain', 'Charles Dickens', 'William Shakespeare', 'Jane Austen'], answer: 'William Shakespeare' },
+    { question: 'What is the smallest prime number?', options: ['0', '1', '2', '3'], answer: '2' },
+  ];
+  
